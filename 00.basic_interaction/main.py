58fe8223c822e68052d1353e8a2dee521bbe2736
Gaetz <gaetan.blaisecazalet@gmail.com>
2018-08-17 16:05:31 +0200

    Separate functions

--- 00.basic_interaction/main.py
+++ 00.basic_interaction/main.py
@@ -1,18 +1,36 @@
-import pygame
 import time
+import pygame
 
-pygame.init()
-screen_size = screen_width, screen_height = 800, 600
-screen = pygame.display.set_mode(screen_size)
+def main():
+    pygame.init()
+    screen, font, text = None, None, None
 
-font = pygame.font.Font(None, 24)
-text = font.render("Hello, World.", True, (255, 126, 0))
+    load()
+    update()
+    draw()
 
-screen.blit(text, (10, 10))
-pygame.display.flip()
-
-time.sleep(2)
-
-pygame.quit()
+    time.sleep(2)
+    pygame.quit()
 
 
+def load():
+    global screen, font, text
+    screen_size = screen_width, screen_height = 800, 600
+    screen = pygame.display.set_mode(screen_size)
+    pygame.display.set_caption("Hello World")
+    font = pygame.font.Font(None, 24)
+    text = font.render("Hello, World.", True, (255, 126, 0))
+
+
+def update():
+    pass
+
+
+def draw():
+    global screen
+    screen.blit(text, (10, 10))
+    pygame.display.update()
+
+
+if __name__ == "__main__":
+    main()
