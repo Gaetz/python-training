242403bf9f064456bbce20c088e22246f7e053c6
Gaetz <gaetan.blaisecazalet@gmail.com>
2018-08-23 16:55:29 +0200

    Start adventure project

---
+++ 01.adventure/02.afficher_sprite/main.py
@@ -0,0 +1,33 @@
+import pygame, time, sys
+
+def main():
+
+    # Load
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    font = pygame.font.Font(None, 24)
+    path = 'D:\\Code\\ArtFx\\Python\\python-training\\01.adventure\\01.afficher_background\\'
+    background = pygame.image.load(path+'background.png').convert()
+    key = False
+    quit = False
+
+    while not(quit):
+        # Inputs
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    quit = False
+
+        # Update
+        pass
+
+        
+        # Draw
+        screen.fill((0, 0, 0))
+        screen.blit(background, (0, 0))
+        pygame.display.update()
+
+if __name__ == "__main__":
+    main()
