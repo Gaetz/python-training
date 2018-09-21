4149d1d49f1b88a0246ddaddbdb734d816418071
Gaetz <gaetan.blaisecazalet@gmail.com>
2018-09-21 15:36:29 +0200

    Animation lesson

---
+++ 01.adventure/28.animation/animation.py
@@ -0,0 +1,21 @@
+import pygame
+
+class Animation:
+    path = 'D:\\Code\\ArtFx\\Python\\python-training\\01.adventure\\28.animation\\images\\'
+    
+    def __init__(self, frame_number, frame_speed, image):
+        self.frame_number = frame_number
+        self.current_frame = 0
+        self.frame_speed = frame_speed
+        self.counter = 0
+        self.surface = pygame.image.load(Animation.path + image).convert_alpha()
+        self.width, self.height = self.surface.get_width() / frame_number, self.surface.get_height()
+
+    def update(self):
+        self.counter = self.counter + 1
+        if self.counter > self.frame_speed:
+            self.counter = 0
+            self.current_frame = (self.current_frame + 1) % self.frame_number
+
+    def get_surface(self):
+        return self.surface.subsurface(pygame.Rect(self.current_frame * self.width, 0, self.width, self.height))
