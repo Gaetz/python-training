19eebec3447ae32e5ef5ce49c3145502708c4818
Gaetz <gaetan.blaisecazalet@gmail.com>
2019-01-10 23:30:11 +0100

    Hero, collisions and move

---
+++ 07.godot-rpg/Hero.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+export (int) var speed = 500
+
+var velocity = Vector2()
+
+func get_input():
+    velocity = Vector2()
+    if Input.is_action_pressed('ui_right'):
+        velocity.x += speed
+    if Input.is_action_pressed('ui_left'):
+        velocity.x -= speed
+    if Input.is_action_pressed('ui_down'):
+        velocity.y += speed
+    if Input.is_action_pressed('ui_up'):
+        velocity.y -= speed
+    velocity = velocity.normalized() * speed
+
+func _physics_process(delta):
+    get_input()
+    move_and_slide(velocity)
